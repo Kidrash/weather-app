46378aadf22476930bc3eba0e4858cf3bcbae28c
Kidrash <markkaranja11@gmail.com>
2022-09-06 11:00:56 +0300

    code for tasks added

--- script.js
+++ script.js
@@ -130,9 +130,31 @@ function fetchWeatherData(){
     .catch(() =>{
         alert("Is that a city?")
         app.style.opacity = '1';
+        
     });
 }
-
+document.addEventListener("DOMContentLoaded", () => {
+    let form =  document.querySelector('form')
+    form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    handleTask(e.target.new_task_description.value)
+    form.reset()
+  })
+  function handleTask(newTask){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+  
+    btn.textContent = "x"
+    btn.addEventListener("click", handleDelete)
+  
+    p.textContent = newTask
+    p.appendChild(btn)
+    document.querySelector('#tasks').appendChild(p);
+  }
+    function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+});
 // call the function on page load
 fetchWeatherData();
 app.style.opacity = 1;
